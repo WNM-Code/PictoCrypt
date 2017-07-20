15805ebd1f80cf7425c68a50367e129f4b093b7b
Will McCall <dev@willmccall.com>
2017-07-19 21:26:10 -0400

    I implemented the permutation idea
    Using a different process, permuting a single row or column then
    applying it to every row or column, I was able to randomize the image
    much more effectively.

--- PictoCrypt/Crypter.cs
+++ PictoCrypt/Crypter.cs
@@ -31,7 +31,7 @@ namespace PictoCrypt
             convKey.Clear();
             keyLen = key.Length;
             char[] splitKey = key.ToCharArray();
-            for (int i = 0; i < key.Length; i++)
+            for (int i = 0; i < keyLen; i++)
             {
                 convKey.Add(splitKey[i]);
             }
@@ -41,10 +41,12 @@ namespace PictoCrypt
         {
             int[] bounds = getBounds(i);
             Bitmap b;
-
-            b = pushV(i, bounds[0], bounds[1]);
+            int[] randx = randomize(bounds[0]);
+            int[] randy = randomize(bounds[1]);
+            b = randomEnW(i, bounds[0], bounds[1], randx);
+            b = randomEnH(b, bounds[0], bounds[1], randy);
+            b = pushV(b, bounds[0], bounds[1]);
             b = pushH(b, bounds[0], bounds[1]);
-            //b = push(i, bounds[0], bounds[1]);
             b.Save(@"" + saveLocation + name + "-encrypted" + type);
             return null;
         }
@@ -53,10 +55,12 @@ namespace PictoCrypt
         {
             int[] bounds = getBounds(i);
             Bitmap b;
-
+            int[] randx = randomize(bounds[0]);
+            int[] randy = randomize(bounds[1]);
             b = unPushH(i, bounds[0], bounds[1]);
             b = unPushV(b, bounds[0], bounds[1]);
-            //b= unPush(i, bounds[0], bounds[1]);
+            b = randomDeH(b, bounds[0], bounds[1], randx);
+            b = randomDeW(b, bounds[0], bounds[1], randy);
             b.Save(@"" + saveLocation + name + "-decrypted" + type);
             return null;
         }
@@ -157,6 +161,166 @@ namespace PictoCrypt
             RectangleF r = i.GetBounds(ref px);
             int[] bounds = { (int)r.Width, (int)r.Height };
             return bounds;
+        }
+
+        //Creates the mapping, utilizing the methods below
+        private int[] randomize(int bound)
+        {
+            ulong[] flat = init(bound);
+            flat = remove(flat);
+            flat = fill(flat);
+            int[] arr = new int[flat.Length];
+            int i = 0;
+            foreach(ulong a in flat)
+            {
+                arr[i] = (int)a -1;
+                i++;
+            }
+            return arr;
+        }
+
+        //Fills the mapping with numbers based on a key that you put into it
+        //this key was held in the containing class and is referenced as "key"
+        private ulong[] init(int longSize)
+        {
+            ulong[] flat = new ulong[longSize];
+            int i = 0;
+            while (i < keyLen && i < longSize)
+            {
+                flat[i] = key[i];
+                i++;
+            }
+            i = 0;
+            int j = keyLen;
+            int l = 0;
+            while (j < longSize)
+            {
+                flat[j] = flat[l] + flat[l + 1];
+                l++;
+                j++;
+            }
+            for (i = 0; i < longSize; i++)
+            {
+                flat[i] = flat[i] % (ulong)longSize;
+            }
+            return flat;
+        }
+
+        //Replaces all duplicates from the mapping with -1
+        private ulong[] remove(ulong[] flat)
+        {
+            int l = 0;
+            int r = flat.Length;
+            while (l < r)
+            {
+                for (int i = l + 1; i < r; i++)
+                {
+                    if (flat[l] == flat[i])
+                    {
+                        flat[i] = 0;
+                    }
+                }
+                l++;
+            }
+            return flat;
+        }
+
+        //Fills the mapping with the missing numbers
+        //Finds the numbers that were missing
+        private ulong[] fill(ulong[] flat)
+        {
+            List<ulong> tempflat = new List<ulong>(flat);
+            List<ulong> tempmissing = new List<ulong>();
+            int m = 0;
+            for (ulong i = 0; i < (ulong)flat.Length; i++)
+            {
+                if (!tempflat.Contains(i+1))
+                {
+                    tempmissing.Add(i+1);
+                    m++;
+                }
+            }
+            ulong[] missing = tempmissing.ToArray();
+            int j = flat.Length-1;
+            int k = 0;
+            for (int i = 0; i < m;)
+            {
+                while (j > 0 && flat[j] != 0)
+                {
+                    j--;
+                }
+                if (j > 0)
+                {
+                    flat[j] = missing[i];
+                    i++;
+                }
+                while (k < flat.Length && flat[k] != 0)
+                {
+                    k++;
+                }
+                if (k <= flat.Length - 1)
+                {
+                    flat[k] = missing[i];
+                    i++;
+                }
+            }
+            return flat;
+        }
+
+        //Method that encrypts the photo with the created mapping
+        private Bitmap randomEnH(Bitmap i, int wid, int hei, int[] key)
+        {
+            Bitmap b = new Bitmap(i);
+            for (int wloc = 0; wloc < wid; wloc++)
+            {
+                for (int hloc = 0; hloc < hei; hloc++)
+                {
+                    b.SetPixel(wloc, key[hloc], i.GetPixel(wloc, hloc));
+                }
+            }
+            return b;
+        }
+
+        //Method that encrypts the photo with the created mapping
+        private Bitmap randomEnW(Bitmap i, int wid, int hei, int[] key)
+        {
+            Bitmap b = new Bitmap(i);
+            for (int hloc = 0; hloc < hei; hloc++) 
+            {
+                for (int wloc = 0; wloc < wid; wloc++)
+                {
+                    b.SetPixel(key[wloc], hloc, i.GetPixel(wloc, hloc));
+                }
+            }
+            return b;
+        }
+
+        //Method that decrypts the photo with the created mapping
+        private Bitmap randomDeH(Bitmap i, int wid, int hei, int[] key)
+        {
+            Bitmap b = new Bitmap(i);
+            for (int hloc = 0; hloc < hei; hloc++)
+            {
+                for (int wloc = 0; wloc < wid; wloc++)
+                {
+                    b.SetPixel(wloc, hloc, i.GetPixel(wloc, key[hloc]));
+                }
+            }
+            return b;
+        }
+
+        //Method that decrypts the photo with the created mapping
+        private Bitmap randomDeW(Bitmap i, int wid, int hei, int[] key)
+        {
+            Bitmap b = new Bitmap(i);
+            for (int hloc = 0; hloc < hei; hloc++)
+            {
+                for (int wloc = 0; wloc < wid; wloc++)
+                {
+                    b.SetPixel(wloc, hloc, i.GetPixel(key[wloc], hloc));
+                }
+            }
+            return b;
         }
     }
 }
